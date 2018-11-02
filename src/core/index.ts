ead7429c76c1e199bcee912b9ba0c4e4206d537b
Reid Mitchell <reidmitchell1@gmail.com>
2018-11-01 17:38:22 -0700

    Clean up index.ts

--- src/core/index.ts
+++ src/core/index.ts
@@ -5,38 +5,26 @@ import { Expectation } from './Expectation';
 export default function createTestRun(testRunOptions?: TestRunOptions) {
   const testRun = new TestRun(testRunOptions);
 
-  const execute = () => testRun.execute();
-  const beforeEach = (func: TestFunc) => testRun.addHook(Hook.beforeEach, func);
-  const afterEach = (func: TestFunc) => testRun.addHook(Hook.afterEach, func);
-  const beforeAll = (func: TestFunc) => testRun.addHook(Hook.beforeAll, func);
-  const afterAll = (func: TestFunc) => testRun.addHook(Hook.afterAll, func);
-
   const describe = (suiteName: string, funcOrOptions: TestFunc | TestOptions, func: TestFunc) => {
-    if (typeof funcOrOptions === 'function') {
-      testRun.addSuite(suiteName, funcOrOptions, {});
-    } else {
-      testRun.addSuite(suiteName, func, funcOrOptions);
-    }
+    const suiteFunc = typeof funcOrOptions === 'function' ? funcOrOptions : func;
+    const options = typeof funcOrOptions === 'function' ? {} : funcOrOptions;
+    testRun.addSuite(suiteName, suiteFunc, options);
   };
 
   const it = (testName: string, funcOrOptions: TestFunc | TestOptions, func: TestFunc) => {
-    if (typeof funcOrOptions === 'function') {
-      testRun.addTest(testName, funcOrOptions, {});
-    } else {
-      testRun.addTest(testName, func, funcOrOptions);
-    }
+    const testFunc = typeof funcOrOptions === 'function' ? funcOrOptions : func;
+    const options = typeof funcOrOptions === 'function' ? {} : funcOrOptions;
+    testRun.addTest(testName, testFunc, options);
   };
 
-  const expect = (obj: any) => new Expectation(obj);
-
   return {
-    execute,
-    beforeEach,
-    afterEach,
-    beforeAll,
-    afterAll,
+    execute: () => testRun.execute(),
+    beforeEach: (func: TestFunc) => testRun.addHook(Hook.beforeEach, func),
+    afterEach: (func: TestFunc) => testRun.addHook(Hook.afterEach, func),
+    beforeAll: (func: TestFunc) => testRun.addHook(Hook.beforeAll, func),
+    afterAll: (func: TestFunc) => testRun.addHook(Hook.afterAll, func),
+    expect: (obj: any) => new Expectation(obj),
     describe,
-    it,
-    expect
+    it
   };
 }
