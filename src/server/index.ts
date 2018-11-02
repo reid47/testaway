f68dd6d5d9e989a40808f2a97036b3312f37be51
Reid Mitchell <reidmitchell1@gmail.com>
2018-11-01 17:30:11 -0700

    Rename

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,7 @@ import TestServer from './TestServer';
 
 const options = {
   port: 3000,
-  rootDir: '/Users/reid/Code/testudo',
+  rootDir: '/Users/reid/Code/testuary',
   testFilePattern: '<rootDir>/notes/**/*.test.js'
 };
 
