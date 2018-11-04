6476f4e3b01a50ee3d5430cd725fbe7f74748cde
Reid Mitchell <reidmitchell1@gmail.com>
2018-11-04 01:08:17 -0700

    Remove unused utils

--- packages/core/src/utils/add-article.ts
+++
@@ -1,5 +0,0 @@
-const beginningVowel = /^[aeiou]/i;
-
-export function addArticle(word: string): string {
-  return beginningVowel.test(word) ? `an ${word}` : `a ${word}`;
-}
--- packages/core/src/utils/pluralize.ts
+++
@@ -1,4 +0,0 @@
-export function pluralize(amount: number, word: string, pluralSuffix: string = 's'): string {
-  const plural = amount !== 1;
-  return plural ? `${word}${pluralSuffix}` : word;
-}
